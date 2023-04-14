6a6f58a8391819559431cce5b828eb6f3c50d377
Maria Magdalena Wichrowska <80290357+aisurumari@users.noreply.github.com>
2023-04-14 14:06:15 +0200

    Update App.js to functional one
    Changing App.js class compontent into functional component

--- src/App.js
+++ src/App.js
@@ -1,57 +1,94 @@
-import { Component } from "react";
+import { Component, useState, useEffect } from "react";
 import "./App.css";
 
 import SearchBox from "./components/search-box/search-box.component";
 import CardList from "./components/card-list/card-list.component";
 
-class App extends Component {
-  constructor() {
-    super();
+const App = () => {
+  const [searchField, setSearchField] = useState("");
+  const [cats, setCats] = useState([]);
+  const [filteredCats, setFilteredCats] = useState([]);
 
-    this.state = {
-      cats: [],
-      searchString: "",
-    };
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((users) =>
-        this.setState(() => {
-          return { cats: users };
-        })
-      );
-  }
+      .then((users) => setCats(users));
+  }, []);
 
-  onSearch = (event) => {
-    const searchString = event.target.value.toLocaleLowerCase();
-
-    this.setState(() => {
-      return { searchString };
+  useEffect(() => {
+    const newFilteredCats = cats.filter((cat) => {
+      return cat.name.toLocaleLowerCase().includes(searchField);
     });
+
+    setFilteredCats(newFilteredCats);
+  }, [cats, searchField]);
+
+  const onSearch = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
   };
 
-  render() {
-    const { cats, searchString } = this.state;
-    const { onSearch } = this;
+  return (
+    <div className="App">
+      <h1 className="app-title">CatFriends</h1>
+      <SearchBox
+        className="search-box"
+        placeholder="search cats"
+        onChangeHandler={onSearch}
+      />
+      <CardList cats={filteredCats} />
+    </div>
+  );
+};
 
-    const filteredcats = cats.filter((cat) => {
-      return cat.name.toLocaleLowerCase().includes(searchString);
-    });
+// class App extends Component {
+//   constructor() {
+//     super();
 
-    return (
-      <div className="App">
-        <h1 className="app-title">CatFriends</h1>
-        <SearchBox
-          className="search-box"
-          placeholder="search cats"
-          onChangeHandler={onSearch}
-        />
-        <CardList cats={filteredcats} />
-      </div>
-    );
-  }
-}
+//     this.state = {
+//       cats: [],
+//       searchString: "",
+//     };
+//   }
+
+//   componentDidMount() {
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//       .then((response) => response.json())
+//       .then((users) =>
+//         this.setState(() => {
+//           return { cats: users };
+//         })
+//       );
+//   }
+
+//   onSearch = (event) => {
+//     const searchString = event.target.value.toLocaleLowerCase();
+
+//     this.setState(() => {
+//       return { searchString };
+//     });
+//   };
+
+//   render() {
+//     const { cats, searchString } = this.state;
+//     const { onSearch } = this;
+
+//     const filteredCats = cats.filter((cat) => {
+//       return cat.name.toLocaleLowerCase().includes(searchString);
+//     });
+
+//     return (
+//       <div className="App">
+//         <h1 className="app-title">CatFriends</h1>
+//         <SearchBox
+//           className="search-box"
+//           placeholder="search cats"
+//           onChangeHandler={onSearch}
+//         />
+//         <CardList cats={filteredCats} />
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
